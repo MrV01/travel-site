7ecb4316dfcea9aeb389fe1e85d2029c274b9767
Vlad V <VladV454@gmail.com>
2017-01-18 18:53:19 -0500

    Added gulpfile.js to the repository

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+gulp.task('default',function() {
+  console.log("You have created GULP task !");
+});
+
+gulp.task('html', function() {
+  console.log("Imafine something useful to be done to your HTML file here!");
+});
