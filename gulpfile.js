87960ef2f964d01c91420fb04d459522bc75dbd8
Vlad V <VladV454@gmail.com>
2017-01-18 23:18:19 -0500

    PostCss Autoprefixer postcss-simple-vars postcss-nested

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,39 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),     // watch  plug-in
+    postcss = require('gulp-postcss'),   //  PostCSS plug-in
+    autoprefixer = require('autoprefixer'),        // Autoprefixer plugin to PostCSS
+    cssvars = require('postcss-simple-vars'),   // Variables in .css files
+    nested = require('postcss-nested');  // Nested CSS rules plug-in for PostCSS
+
 gulp.task('default',function() {
-  console.log("You have created GULP task !");
+  console.log("You have created GULP task: default !");
 });
 
 gulp.task('html', function() {
-  console.log("Imafine something useful to be done to your HTML file here!");
+  console.log("Imagine something useful to be done to your HTML file here!");
+});
+
+
+gulp.task('styles', function() {
+  // PostCSS attached to GULP work flow
+  // pipes in gulp:  gulp.src(./main.css) -->  gulp.dest(./temp/main.css)
+  // By the way, gulp.src() is asyncronus function
+  return gulp.src('./app/assets/styles/styles.css') // source files
+    .pipe(postcss([cssvars, nested, autoprefixer]))  // filter hook for POSTCSS to include
+                                          // postcss-simple-vars postcss-nested autoprefixer
+    .pipe(gulp.dest('./app/temp/styles')); // destination directory
+  // console.log("Imagine SASS or POSTCSS done to your CSS file's here!");
+});
+
+
+// gulp watch  command to watch for files modifications
+gulp.task('watch', function() {
+  // trigger task "html"  when ./app/index.html changed
+    watch('./app/index.html', function(){
+      gulp.start('html');
+    });
+    // trigger task "styles"  when ./app/index.html changed
+    watch('./app/assets/styles/**/*.css', function(){
+       gulp.start('styles');
+    });
 });
