9b65a3881913656d9cd1a6abda2ea34eb8a93038
Vlad <VladV454@gmail.com>
2017-02-21 00:38:00 -0500

    Up to What is BEM chapter. POSTCSS import and CSS files structure have been intoruced

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,25 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),   //  PostCSS plug-in
     autoprefixer = require('autoprefixer'),        // Autoprefixer plugin to PostCSS
     cssvars = require('postcss-simple-vars'),   // Variables in .css files
-    nested = require('postcss-nested');  // Nested CSS rules plug-in for PostCSS
+    nested = require('postcss-nested'),   // Nested CSS rules plug-in for PostCSS
+// Lession 10 has been ended at "postcss-nested" plugin point
+    cssImport = require('postcss-import');
 
 gulp.task('default',function() {
   console.log("You have created GULP task: default !");
 });
 
 gulp.task('html', function() {
-  console.log("Imagine something useful to be done to your HTML file here!");
+  console.log("HTML files task!");
 });
 
 gulp.task('styles', function() {
- console.log("Imagine SASS or POSTCSS done to your CSS file's here!");
+ console.log("POSTCSS to CSS file's!");
   // PostCSS attached to GULP work flow
   // pipes in gulp:  gulp.src(./main.css) -->  gulp.dest(./temp/main.css)
   // By the way, gulp.src() is asyncronus function
   return gulp.src('./app/assets/styles/styles.css') // source files
-    .pipe(postcss([cssvars, nested, autoprefixer]))  // filter hook for POSTCSS to include
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))  // filter hook for POSTCSS to include
                                           // postcss-simple-vars postcss-nested autoprefixer
     .pipe(gulp.dest('./app/temp/styles')); // destination directory
 });
@@ -31,7 +33,7 @@ gulp.task('watch', function() {
     watch('./app/index.html', function(){
       gulp.start('html');
     });
-    // trigger task "styles"  when ./app/index.html changed
+    // trigger task "styles"  when a /styles/**/*.css file(s)  changed
     watch('./app/assets/styles/**/*.css', function(){
        gulp.start('styles');
     });
