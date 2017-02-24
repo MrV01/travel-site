b587a58d090e2939cb35631927fa4dc8c8fcce82
Vlad <VladV454@gmail.com>
2017-02-24 11:36:53 -0500

    Organized - split gulpfile.js into two task files. Added browserSync functionality - Nicegit status!

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,35 @@
+// Gulp watch task
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),     // watch  plug-in
+    browserSync = require('browser-sync').create(); // Only create() method from the package.
+
+// "gulp watch"  command to watch for HTML,CSS files modifications
+gulp.task('watch', function() {
+
+  // browserSync spins up a little server
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  // trigger task "html"  when ./app/index.html changed
+    watch('./app/index.html', function(){
+      // dummy task "gulp.start('html');" has been removed.
+      // reload browserSync, whenever index.html changed
+      browserSync.reload();
+    });
+    // trigger task "styles"  when a /styles/**/*.css file(s)  changed
+    watch('./app/assets/styles/**/*.css', function(){
+       // gulp.start('styles');
+       gulp.start('cssInject');
+    });
+});
+
+// Browser-sync injection of CSS upon change of tmp/styles/styles.css
+gulp.task('cssInject', ['styles'] , function(){
+  // Inject new CSS content "on the fly"
+  return gulp.src('./app/temp/styles/styles.css')
+      .pipe(browserSync.stream());
+});
