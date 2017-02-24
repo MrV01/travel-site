5ce49d498b159fc98785fe79eea175a55095ee29
Vlad <VladV454@gmail.com>
2017-02-24 12:32:22 -0500

    Mobile first approach. Postcss-mixins module introduced and used for multiple screen sizes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),        // Autoprefixer plugin to PostCSS
   cssvars = require('postcss-simple-vars'),   // Variables in .css files
   nested = require('postcss-nested'),   // Nested CSS rules plug-in for PostCSS
-  cssImport = require('postcss-import');
+  cssImport = require('postcss-import'),
+  mixins = require('postcss-mixins') ; // postcss-mixins module.
 
 // GULP CSS styles conversion/processing
 gulp.task('styles', function() {
@@ -13,7 +14,7 @@ gulp.task('styles', function() {
   // pipes in gulp:  gulp.src(./main.css) -->  gulp.dest(./temp/main.css)
   // By the way, gulp.src() is asyncronus function
   return gulp.src('./app/assets/styles/styles.css') // source files
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))  // filter hook for POSTCSS to include
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))  // filter hook for POSTCSS to include
         // postcss-simple-vars postcss-nested autoprefixer
     .on('error', function(errorInfo) { // stop interrupting work flow due to css errors
       console.log(errorInfo.toString());
