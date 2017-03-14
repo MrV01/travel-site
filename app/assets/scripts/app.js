d9a06e81e41dc9cd2a483390b7d80d276be11352
Vlad <VladV454@gmail.com>
2017-03-13 20:32:28 -0400

    Styling site-header section, Start super organized javascript part 2

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,16 @@
+// first JavaScript experience
+function person(name , color) {
+  console.log("Hello my name is " + name + " and my favorite color is " + color );
+}
+person("John Doe", "blue");
+person("Jane Doe", "red");
+person("Johnny Five", "green");
+
+var john = {
+  name: "John Doe",
+  favoriteColor: "blue",
+  greet: function() {
+
+  }
+
+}
