c2d052d9c69e73cf4f79e587cd862b753726db4b
Vlad <VladV454@gmail.com>
2017-03-22 13:30:07 -0400

    Completed Highlight Current Page Section link, and Smooth Scroll. Next: create own Modal or Light box, or Overlay

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,7 +1,8 @@
 // Module StickyHeader
 import $ from 'jquery';
-import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints'; // Lib does not have main
-
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+              // Lib does not have main
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
@@ -9,6 +10,17 @@ class StickyHeader {
     this.siteHeader = $(".site-header");
     this.headerTriggerElement = $(".large-hero__title");
     this.createHeaderWaypoint();
+    // feature of sticky header: Highlight Current Page Section
+    this.pageSections = $(".page-section"); // all CSS class="page-section"
+    this.headerLinks = $(".primary-nav a"); // list of all primary-nav elements  anchors
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  // Smooth Scroll  npm package: npm install jquery-smooth-scroll --save
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
   }
 
   // trigger Waypoint on the  first <h1> title
@@ -26,6 +38,44 @@ class StickyHeader {
       }
     });
   }
+
+  // Current page section Highlight feature
+
+  createPageSectionWaypoints() {
+    var that = this;
+    this.pageSections.each(function() {
+      var currentPageSection = this;
+      new Waypoint({
+        element: currentPageSection,
+        handler: function( direction ) {
+          if(direction == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            // Remove class="is-current-link" from the other Page sections first
+            that.headerLinks.removeClass("is-current-link");
+            // Add class="is-current-link" to the active section
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "18%"  // by  default 0% - top of the window
+      });
+
+      new Waypoint( {   // Scrolling up waypoints
+        element: currentPageSection,
+        handler: function( direction ){
+          if(direction == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            // Remove class="is-current-link" from the other Page sections first
+            that.headerLinks.removeClass("is-current-link");
+            // Add class="is-current-link" to the active section
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "-41%"  // by  default 0% - top of the window
+      });
+
+    });
+
+  } // class
 
 }
 
